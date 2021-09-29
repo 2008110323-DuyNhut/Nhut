8d45acd5d445d72a268544d95b59c0df465f102a
2008110323-DuyNhut <nguyenduynhut2002@gmail.com>
2021-09-29 21:48:49 +0700

    Lap3

---
+++ src/Lap3/bai2Lap3.java
@@ -0,0 +1,21 @@
+package Lap3;
+import java.util.Scanner;
+
+public class bai2Lap3 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i = 1;i <= 10;i++)
+        {
+            System.out.println("Đây là chương trình in bảng nhân!");
+            System.out.printf("Bảng Nhân " +i);
+            for(j = 1;j < 10;j++)
+            {
+                System.out.printf("\n%d x %d = %d\n",i,j,i*j);
+            }
+    }
+}
+}
+  
+
+    
+
